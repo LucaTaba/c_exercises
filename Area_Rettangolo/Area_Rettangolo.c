a95172374e07c5e812cb7a8e68e0b7606e5ac404
LucaTaba <tabariniluca@email.com>
2025-03-24 19:55:50 +0100

    Esercizio_area_rettangolo

---
+++ Area_Rettangolo/Area_Rettangolo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int base;
+    int altezza;
+
+    printf("Inserisci la base: ");
+    scanf("%d", &base);
+
+    printf("Inserisci la altezza: ");
+    scanf("%d", &altezza);
+
+    printf("Inserisci un numero intero: %d ", base*altezza);
+
+  return 0;
+}
---
+++ Area_Rettangolo/Readme.txt
@@ -0,0 +1,10 @@
+Esercizio base per il calcolo dell'area di un rettangolo
+
+
+Per compilare:
+
+    gcc Area_Rettangolo.c -o Area_Rettangolo
+
+Per eseguire:
+
+    .\Area_rettangolo.exe
